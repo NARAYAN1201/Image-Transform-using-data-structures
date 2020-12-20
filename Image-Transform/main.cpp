cfc17058ff9cc539a724363bb71fa05953f9c1b9
Narayan <narayankaushik16@gmail.com>
2020-12-20 16:55:42 +0530

    All Effects

--- Image-Transform/main.cpp
+++ Image-Transform/main.cpp
@@ -9,20 +9,20 @@
 int main() {
   uiuc::PNG png, png2, result;
 
-  png.readFromFile("IMG_20170501_220755.png");
-  //  uncomment for other effects result.
-  //result = grayscale(png);
-  //result.writeToFile("out-grayscale.png");
+  png.readFromFile("alma.png");
 
-  //result = createSpotlight(png, 450, 150);
-  //result.writeToFile("out-spotlight.png");
+  result = grayscale(png);
+  result.writeToFile("out-grayscale.png");
+
+  result = createSpotlight(png, 450, 150);
+  result.writeToFile("out-spotlight.png");
 
   result = illinify(png);
   result.writeToFile("out-illinify.png");
 
-  //png2.readFromFile("overlay.png");
-  //result = watermark(png, png2);
-  //result.writeToFile("out-watermark.png");
+  png2.readFromFile("overlay.png");
+  result = watermark(png, png2);
+  result.writeToFile("out-watermark.png");
 
   return 0;
 }
