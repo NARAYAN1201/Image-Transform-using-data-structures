f541f59d37d37c4f6643a5d0b0ffd590c414e8bb
NARAYAN <57436743+NARAYAN1201@users.noreply.github.com>
2020-12-20 16:31:36 +0530

    Update HSLAPixel.h

--- Image-Transform/uiuc/HSLAPixel.h
+++ Image-Transform/uiuc/HSLAPixel.h
@@ -1,8 +1,5 @@
 /**
  * @file HSLAPixel.h
- *
- * @author University of Illinois CS 225 Course Staff
- * @version 2018r1-lab1 - Updated for CS 400
  */
 
 #pragma once
@@ -26,7 +23,7 @@ namespace uiuc {
         HSLAPixel();   // defalut constructor
 
 
-        HSLAPixel(double hue, double saturation, double luminance); // when alpha =1.0 copy constructor
+        HSLAPixel(double hue, double saturation, double luminance); // when alpha = 1.0 copy constructor
 
 
         HSLAPixel(double hue, double saturation, double luminance, double alpha);  // copy constructor
