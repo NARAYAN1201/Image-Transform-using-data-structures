10e8b2c7c8e66a51bcc58d2a1d577ddd8d789b4c
NARAYAN <57436743+NARAYAN1201@users.noreply.github.com>
2020-12-20 16:30:20 +0530

    Update ImageTransform.h

--- Image-Transform/ImageTransform.h
+++ Image-Transform/ImageTransform.h
@@ -2,7 +2,7 @@
 
 #include "uiuc/PNG.h"
 using namespace uiuc;
-
+// Defining the effects to be done on the image.
 PNG grayscale(PNG image);  
 PNG createSpotlight(PNG image, int centerX, int centerY);
 PNG illinify(PNG image);
